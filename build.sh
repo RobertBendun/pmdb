96e4ccf6040b05c30fb6328761c73381376086b9
Robert Bendun <rob.bendun@gmail.com>
2022-11-20 13:56:02 +0100

    remove previous database only if it exists

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ DATABASE="pmdb.db"
 
 (cd ./db-tool/; go build)
 
-rm "$DATABASE"
+[ -e "$DATABASE" ] && rm "$DATABASE"
 cat tables/*.sql | sqlite3 "$DATABASE"
 ./db-tool/db-tool import --db "$DATABASE" --table personal_ratings --src ratings.csv --delim ','
